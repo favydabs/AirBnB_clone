59a9fbae318940b224d28a7b9d063497b3f19f29
Henry <orjihen@yahoo.com>
2023-05-15 15:24:07 +0000

    bug fix - Store first object.

--- models/base_model.py
+++ models/base_model.py
@@ -28,13 +28,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         """
         Updates the updated_at attribute with the current datetime
         """
         self.updated_at = datetime.now()
-        storage.new(self)
         storage.save()
 
     def to_dict(self):
