31f7c85a05c140cde7bd0c932dab93ea5dd2aaac
Henry <orjihen@yahoo.com>
2023-05-15 14:36:11 +0000

    Added Amenity and Review classes to project

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defines Amenity class and,
+Initializes attributes that represents Amenity
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+
+    """ amenity attributes """
+    name = ""
